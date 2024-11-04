d3021de4a7f7f18c97274ca390098416f9d365f3
Pankaj <pankaj.awasthi@novoinvent.com>
2024-11-04 20:43:14 +0530

    updates in summary screen

--- lib/models/results_screen.dart
+++ lib/models/results_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quizapp/data/questions.dart';
-
+import 'package:flutter_quizapp/models/questions_summary.dart';
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key, required this.chosenAnswers});
+  const ResultsScreen({
+    super.key, 
+    required this.chosenAnswers
+    });
 
   final List<String> chosenAnswers;
 
@@ -24,19 +27,32 @@ for(var i =0; i < chosenAnswers.length; i++){
 
   @override
   Widget build(BuildContext context) {
+   final summaryData = getSummaryData();
+   final numTotalQuestions = questions.length;
+   //filter list and return new list
+   final numCorrectQuestions = summaryData.where((data){
+   return data['user_answer'] == data['correct_answer'];
+   }).length;//amount of item kept in flitered list
+
     return SizedBox(
       width: double.infinity,
       child: Container(
         margin: const EdgeInsets.all(20),
-        child: const Column(
+        child:  Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("You answered X out of Y questions correctly"),
-            SizedBox(
+             Text("You answered ${numCorrectQuestions} out of ${numTotalQuestions} questions correctly"),
+           
+           const SizedBox(
               height: 30,
             ),
-            Text("List of answers and questions..."),
-            TextButton(onPressed: () {}, child: Text('Restart Quiz'))
+             QuestionsSummary(summaryData),
+          const  SizedBox(
+              height: 30,
+            ),
+            const Text("List of answers and questions..."),
+            TextButton(onPressed: () {}, 
+            child: const Text('Restart Quiz'))
           ],
         ),
       ),
